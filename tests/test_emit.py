4314e72af3977afe0c1d8c195be74534541d935f
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-10-05 12:13:30 +0300

    Even more tests

--- tests/test_emit.py
+++ tests/test_emit.py
@@ -23,3 +23,22 @@ async def test_emit_full_queue(setup_logger, loop, mocker):
     m_log.warning.assert_called_with(
         'Queue is full, drop oldest message: "%(record)s"',
         {'record': mock.ANY})
+
+
+async def test_emit_unexpected_err_in_worker(setup_logger, loop, mocker):
+    log, hdlr, srv = await setup_logger()
+
+    err = RuntimeError()
+    fut = asyncio.Future(loop=loop)
+
+    async def coro(record):
+        fut.set_result(None)
+        raise err
+
+    hdlr._send = coro
+    m_log = mocker.patch('aiologstash.base_handler.logger')
+
+    log.info('Msg')
+    await fut
+    m_log.warning.assert_called_with('Unexpected Exception while sending log',
+                                     exc_info=err)
